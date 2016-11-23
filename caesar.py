3ac26fb5b48d518c2a205e1f0c9c948103cbae4a
patjm1992 <patjm1992@gmail.com>
2016-11-23 18:33:28 -0500

    Now works for all characters and preserves punctuation

--- caesar.py
+++ caesar.py
@@ -1,9 +1,13 @@
-alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+import timeit
+import string
+
+
+alphabet = string.printable
 
 def encrypt(x):
     i = 0
     while i < len(x):
-        if x[i] != ' ':
+        if x[i] != ' ' and x[i] not in string.punctuation:
             a = 0
             while ord(alphabet[a]) != ord(x[i]):
                 a += 1
@@ -14,7 +18,7 @@ def encrypt(x):
 def decrypt(x):
     i = 0
     while i < len(x):
-        if x[i] != ' ':
+        if x[i] != ' ' and x[i] not in string.punctuation:
             a = 0
             while ord(alphabet[a]) != ord(x[i]):
                 a += 1
@@ -22,55 +26,65 @@ def decrypt(x):
         i += 1
     return x
 
-#User begins by choosing encrytion or decrytion
 choice = input("Enter (1) to encrypt a message or (2) to decrypt a message: ")
 
 while choice != 1 and choice != 2:
     choice = input("Enter (1) to encrypt a message or (2) to decrypt a message: ")
 
 if choice == 1:
-    
-    #plaintext encryption
     plaintext = raw_input("Enter plaintext message (letters and spaces only): ")
     plaintext = plaintext.upper()
     p = list(plaintext)
 
     s = input("Enter shift parameter: ")
 
+    start_time = timeit.default_timer()
     encrypt(p)
     ciphertext = ''.join(p)
+    elapsed = timeit.default_timer() - start_time
     print ciphertext
+    print("Encrypted message in %.10f seconds." % elapsed)
 
 if choice == 2:
-
-    #ciphertext decryption
-    
     ciphertext = raw_input("Enter ciphertext message: ")
     ciphertext = ciphertext.upper()
     c = list(ciphertext)
 
-    #is the key known?
-    known_key = raw_input("Do you know the shift parameter? Enter Yes or No: ")
+    known_key = raw_input("Do you know the shift parameter? Enter (Y)es or (N)o: ")
 
-    while known_key != "Yes" and known_key != "yes" and known_key != "Y" and known_key != "y" and known_key != "No" and known_key != "no" and known_key != "N" and known_key != "n":
+    while (known_key != "Yes"
+        and known_key != "yes"
+        and known_key != "Y"
+        and known_key != "y"
+        and known_key != "No"
+        and known_key != "no"
+        and known_key != "N"
+        and known_key != "n"):
         known_key = raw_input("Do you know the shift parameter? Enter Yes or No: ")
-        
-    #if the key is already known
-    if known_key == "Yes" or known_key == "yes" or known_key == "Y" or known_key == "y":
-        
+
+    if (known_key == "Yes" or known_key == "yes" or known_key == "Y" or known_key == "y"):
+
         s = input("Enter shift parameter: ")
 
+        start_time = timeit.default_timer()
         decrypt(c)
         plaintext = ''.join(c)
         print plaintext
 
-    #if the key is not known, we can try brute force
+        elapsed = timeit.default_timer() - start_time
+        print("Decrypted with known key in %.10f seconds." % elapsed)
+
     else:
-        
-        s = 0
+        s = 1
+        print ""
+
+        start_time = timeit.default_timer()
         while s < 26:
             decrypt(c)
             plaintext = ''.join(c)
             print plaintext
             encrypt(c)
             s += 1
+
+        elapsed = timeit.default_timer() - start_time
+        print("\nDecrypted by brute force method in %.10f seconds." % elapsed)
